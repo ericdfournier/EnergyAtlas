50d97be921156bf09ce4bcb93048aefcdae7d93b
Eric Daniel Fournier <me@ericdfournier.com>
2016-11-28 15:26:28 -0800

    Improved Function Modularization
    Decomposed several composite functions into more autonomous
    sub-routines to facilitate debugging.

--- selection_histogram.py
+++ selection_histogram.py
@@ -11,18 +11,69 @@ from bokeh.layouts import (
 from bokeh.models import (
     ColumnDataSource,
     BoxSelectTool, 
-    LassoSelectTool, 
+    LassoSelectTool,
+    HoverTool,
+    LogColorMapper,
     Spacer,
     Select,
-    Range1d,
+    DataRange1d,
     PreText
     )
 from bokeh.plotting import (
     figure, 
     curdoc,
     )
+from bokeh.palettes import Viridis6 as palette
 
-#%% create three normal population samples with different parameters
+#%% load map datasource
+
+path = "/Users/edf/Repositories/EnergyAtlas/data/la_county_neighborhood_boundaries_single.json"
+map_source = pd.read_json(path, typ='series', orient='column')
+neighborhoods = {}
+
+#%% clean raw map datasource
+
+for i,feature in enumerate(map_source.features):
+    
+    if feature['geometry']['type'] == 'Polygon':
+        
+        coords = feature['geometry']['coordinates'][0]
+        name = feature['properties']['Name'][:]
+        lon,lat,_ = np.reshape(coords,[len(coords),3]).T
+        neighborhoods[i] = {}
+        neighborhoods[i]['lat'] = lat
+        neighborhoods[i]['lon'] = lon
+        neighborhoods[i]['name'] = name
+    
+    # There is still an issue with the parsing of multi-part polygons
+    
+    elif feature['geometry']['type'] == 'MultiPolygon':
+        
+        coords = feature['geometry']['coordinates'][:]
+        name = feature['properties']['Name'][:]
+        lons = []
+        lats = []
+        
+        for j, poly in enumerate(coords):
+            
+            lon,lat,_ = np.reshape(coords[j][0],[len(coords[j][0]),3]).T
+            # There is a nesting list issue here that might be resolved in the future
+            lons.append(lon)
+            lats.append(lat)
+        
+        neighborhoods[i] = {}
+        neighborhoods[i]['lat'] = lats
+        neighborhoods[i]['lon'] = lons
+        neighborhoods[i]['name'] = name
+
+#%% Prep Source Data for Plotting
+
+neighborhood_lon = [neighborhood["lon"] for neighborhood in neighborhoods.values()]
+neighborhood_lat = [neighborhood["lat"] for neighborhood in neighborhoods.values()]
+neighborhood_names = [neighborhood['name'] for neighborhood in neighborhoods.values()]
+color_mapper = LogColorMapper(palette=palette)
+
+#%% Generate Random Energy Data for Visualization
 
 seed = (12345)
 np.random.seed(seed)
@@ -35,6 +86,21 @@ x_rnd,y_rnd = np.exp(np.random.multivariate_normal(mu, sigma, size).T)
 x_rnd_y_rnd = np.divide(x_rnd,y_rnd)
 year_rnd = np.random.randint(1850,2010,size)
 
+unique_names = list(set(neighborhood_names))
+neighborhoods = [None]*size
+
+# TODO: FIX DEPRECATED INDEXING BELOW
+for i in range(size):
+    ind = np.random.randint(0,len(unique_names),1,dtype=int)
+    neighborhoods[i] = unique_names[ind]
+
+neigh_avg = np.zeros(len(unique_names),dtype=float)
+for j in range(len(neigh_avg)):
+    ind = [i for i, x in enumerate(neighborhoods) if x == unique_names[j]]
+    neigh_avg[j] = np.average(y_rnd[ind])
+    
+#%% create reference data frame
+
 df = pd.DataFrame()
 
 df['size'] = x_rnd
@@ -42,11 +108,7 @@ df['consumption'] = y_rnd
 df['intensity'] = x_rnd_y_rnd
 df['year'] = year_rnd
 
-#%% create stats
-
-stats = PreText(text='', width=500)
-
-#%% create input controls
+#%% create axis map
 
 axis_map = {
     "Energy Consumption [MBTU]": "consumption",
@@ -55,23 +117,55 @@ axis_map = {
     "Construction Vintage [Year]" : "year"
 }
 
+#%% create input controls
+
 x_axis = Select(title='X-Axis', value='Building Size [sq.ft.]', options=sorted(axis_map.keys()))
 y_axis = Select(title='Y-Axis', value='Energy Consumption [MBTU]', options=sorted(axis_map.keys()))
 
-#%% create columnar data source
+#%% create map columnar data source
 
-source = ColumnDataSource(data=dict(x=[],y=[]))
+map_source = ColumnDataSource(data=dict(
+    lon = neighborhood_lon,
+    lat = neighborhood_lat,
+    avg_consumption = neigh_avg,
+    name = neighborhood_names,
+))
 
-source.data = dict(
+#%% create plot columnar data source
+
+plot_source = ColumnDataSource(data=dict(
     x = df[axis_map[x_axis.value]].values,
     y = df[axis_map[y_axis.value]].values
-)
+))
+
+#%% create stats
+
+stats = PreText(text='', width=500)
+
+#%% create map plot
+
+MAP_TOOLS="pan,wheel_zoom,reset,hover,save"
+
+m = figure(title="Los Angeles County Neighborhoods", tools=MAP_TOOLS, plot_width=450, plot_height=550, min_border=10, x_axis_location=None, y_axis_location=None,webgl=True)
+m.grid.grid_line_color = None
+
+mp = m.patches('lon','lat', source=map_source,
+          fill_color={'field':'avg_consumption','transform':color_mapper},
+          line_color="white", line_width=0.5)
+          
+hover = m.select_one(HoverTool)
+hover.point_policy = "follow_mouse"
+hover.tooltips = [
+    ("Name", "@name"),
+    ("(Long, Lat)", "($x, $y)"),
+    ("Mean Annual Energy Consumption [MBTU / Megaparcel]", "@avg_consumption")
+]
 
 #%% create the scatter plot
 
-TOOLS="pan,wheel_zoom,box_zoom,box_select,lasso_select,reset"
+PLOT_TOOLS="pan,wheel_zoom,box_zoom,box_select,lasso_select,reset"
 
-p = figure(tools=TOOLS, plot_width=600, plot_height=600, min_border=50, 
+p = figure(tools=PLOT_TOOLS, plot_width=600, plot_height=600, min_border=50, 
        min_border_left=50, toolbar_location="right", 
        x_axis_location="above", y_axis_location="left", 
        title="Linked Histograms", webgl=True)
@@ -80,11 +174,11 @@ p.background_fill_color = None
 p.select(BoxSelectTool).select_every_mousemove = False
 p.select(LassoSelectTool).select_every_mousemove = False    
 
-r = p.scatter('x', 'y', source=source, size=3, color="#3A5785", alpha=0.6, selection_color="orange")
+r = p.scatter('x', 'y', source=plot_source, size=3, color="#3A5785", alpha=0.6, selection_color="orange")
 
 # %% create the horizontal histogram
 
-x = np.array(source.data['x'])
+x = np.array(plot_source.data['x'])
 
 hhist, hedges = np.histogram(x, bins=20)
 hzeros = np.zeros(len(hedges)-1)
@@ -110,7 +204,7 @@ hh2 = ph.quad(bottom=0, left=hedges[:-1], right=hedges[1:], top=hzeros,
 
 #%% create the vertical histogram
 
-y = np.array(source.data['y'])
+y = np.array(plot_source.data['y'])
 
 vhist, vedges = np.histogram(y, bins=20)
 vzeros = np.zeros(len(vedges)-1)
@@ -131,54 +225,73 @@ vh1 = pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vzeros,
 vh2 = pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vzeros, 
               alpha=0.1, **LINE_ARGS_2)
 
-#%% Update Histograms
+#%% Update Major X Histograms
 
-def update_major_histograms(source):
+def update_major_x_histogram(source):
     
     hhist0, hedges0 = np.histogram(np.array(source.data['x']), bins=20)
-    vhist0, vedges0 = np.histogram(np.array(source.data['y']), bins=20)
     
     hh0.data_source.data['right'] = hedges0[1:]
     hh0.data_source.data['left'] = hedges0[:-1]
     hh0.data_source.data['top'] = hhist0
 
+    hmax0 = max(hhist0)
+    hmin0 = min(hhist0)    
+    space = 0.1
+    
+    ph.set(y_range = DataRange1d(hmax0, space, hmin0))
+    
+#%% Update Major Y Histograms
+
+def update_major_y_histogram(source):
+    
+    vhist0, vedges0 = np.histogram(np.array(source.data['y']), bins=20)
+
     vh0.data_source.data['top'] = vedges0[1:]
     vh0.data_source.data['bottom'] = vedges0[:-1]
     vh0.data_source.data['right'] = vhist0
+
+    vmax0 = max(vhist0)
+    vmin0 = min(vhist0)
+    space = 0.1
+    
+    pv.set(x_range = DataRange1d(vmax0, space, vmin0))
 
 #%% Update X-axis
     
 def update_x_axis(attr, old, new):
                 
-    source.data['x'] = df[axis_map[new]].values
+    plot_source.data['x'] = df[axis_map[new]].values
     
-    minx = min(source.data['x'])
-    maxx = max(source.data['x'])
+    minx = min(plot_source.data['x'])
+    maxx = max(plot_source.data['x'])
+    space = 0.1
     
-    p.x_range = Range1d(minx-(0.1*maxx),maxx+(0.1*maxx))
-
+    p.set(x_range = DataRange1d(maxx, space, minx))
+    ph.set(x_range = p.x_range)
     ph.xaxis.axis_label = new
-    ph.x_range = p.x_range
     
-    update_major_histograms(source)
+    update_major_x_histogram(plot_source)
+    update_minor_x_histogram(inds)
     
 #%% Update Y-axis
 
 def update_y_axis(attr, old, new):
     
-    source.data['y'] = df[axis_map[new]].values
+    plot_source.data['y'] = df[axis_map[new]].values
     
-    miny = min(source.data['y'])
-    maxy = max(source.data['y'])
+    miny = min(plot_source.data['y'])
+    maxy = max(plot_source.data['y'])
+    space = 0.1
     
-    p.y_range = Range1d(miny-(0.1*maxy),maxy+(0.1*maxy))
-    
+    p.set(y_range = DataRange1d(maxy, space, miny))
+    pv.set(y_range = p.y_range)
     pv.yaxis.axis_label = y_axis.value
-    pv.y_range = p.y_range
-    
-    update_major_histograms(source)
 
-#%% Update Data
+    update_major_y_histogram(plot_source)
+    update_minor_y_histogram(inds)
+
+#%% Update Stats Panel
 
 def update_stats(inds):
         
@@ -186,31 +299,49 @@ def update_stats(inds):
         stats.text = str(df.describe())
     else:
         stats.text = str(df.ix[inds].describe())
-        
-#%% Update Histograms
 
-def update_minor_histograms(inds):
+#%% Upated Minor x Histogram
+
+def update_minor_x_histogram(inds):
         
     if len(inds) == 0 or len(inds) == len(df['size'].values):
         hhist1, hhist2 = hzeros, hzeros
-        vhist1, vhist2 = vzeros, vzeros
     else:
-        neg_inds = np.ones_like(np.array(source.data['x']), dtype=np.bool)
+        neg_inds = np.ones_like(np.array(plot_source.data['x']), dtype=np.bool)
         neg_inds[inds] = False
-        
-        x = np.array(source.data['x'])
-        y = np.array(source.data['y'])
-
+        x = np.array(plot_source.data['x'])
         hhist1, _ = np.histogram(x[inds], bins=hedges)
         hhist2, _ = np.histogram(x[neg_inds], bins=hedges)
+
+    hh1.data_source.data["top"] =  hhist1
+    hh2.data_source.data["top"] = -hhist2
+        
+#%% Update Minor y Histogram
+
+def update_minor_y_histogram(inds):
+        
+    if len(inds) == 0 or len(inds) == len(df['size'].values):
+        vhist1, vhist2 = vzeros, vzeros
+    else:
+        neg_inds = np.ones_like(np.array(plot_source.data['y']), dtype=np.bool)
+        neg_inds[inds] = False
+        y = np.array(plot_source.data['y'])
         vhist1, _ = np.histogram(y[inds], bins=vedges)
         vhist2, _ = np.histogram(y[neg_inds], bins=vedges)
 
-    hh1.data_source.data["top"]   =  hhist1
-    hh2.data_source.data["top"]   = -hhist2
     vh1.data_source.data["right"] =  vhist1    
     vh2.data_source.data["right"] = -vhist2
 
+#%% Update Map
+
+#def update_map(inds):
+#    
+#    if len(inds) < 100:
+#        # Decolor map
+#    else:
+#       # Color map on the basis of selected points
+#       print('test')
+        
 #%% Update Selection    
     
 def update_selection(attr, old, new):
@@ -218,14 +349,15 @@ def update_selection(attr, old, new):
     inds = np.array(new['1d']['indices'], dtype=int)
         
     update_stats(inds)
-    update_minor_histograms(inds)
-
-#%% Create Selection Widgets
+    update_minor_x_histogram(inds)
+    update_minor_y_histogram(inds)
+    
+    #%% Create Selection Widgets
 
 sizing_mode = 'fixed' 
 
 widgets = widgetbox([x_axis, y_axis], sizing_mode=sizing_mode)
-layout = row(column(widgets,stats), column(row(p, pv), row(ph, Spacer(width=200, height=200))))
+layout = row(column(widgets,stats,m), column(row(p, pv), row(ph, Spacer(width=200, height=200))))
 
 curdoc().add_root(layout)
 curdoc().title = "Selection Histogram"
@@ -233,6 +365,8 @@ curdoc().title = "Selection Histogram"
 x_axis.on_change('value', update_x_axis)
 y_axis.on_change('value', update_y_axis)
 r.data_source.on_change('selected', update_selection)
+
+inds = np.array([],dtype=int)
 
 # TODO: Integrate slider functionality into selection update callback
 
